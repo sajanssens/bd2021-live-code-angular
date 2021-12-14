3091b9922bcef5f51a92b81c00d7c50858670eba
bramjanssens <bram.janssens@infosupport.com>
2021-12-14 22:01:34 +0100

    Checkboxes now dynamic.

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {ContactService} from '../../services/contact.service';
 import {Contact} from '../../domain/contact';
 import {Gender} from '../../domain/gender';
@@ -15,42 +15,51 @@ export class ContactFormComponent implements OnInit {
   @Input() contact: Contact;
 
   // tslint:disable-next-line:variable-name
-  private _genders: Gender[] = [{id: 1, name: 'Male'}, {id: 2, name: 'Female'}];
+  private _genders: Gender[] = [
+    {id: 1, name: 'Male'},
+    {id: 2, name: 'Female'}
+  ];
 
   // tslint:disable-next-line:variable-name
-  private _languages: Language[] = [{id: 1, name: 'Java'}, {id: 2, name: 'Kotlin'}, {id: 3, name: 'Typescript'}, {id: 4, name: 'PHP'},];
+  languages: Language[] = [
+    {id: 1, name: 'Java'},
+    {id: 2, name: 'Kotlin'},
+    {id: 3, name: 'Typescript'},
+    {id: 4, name: 'PHP'}
+  ];
 
   contactForm: FormGroup;
   plusMinus = 'plus';
 
-  constructor(private contactService: ContactService, private fb: FormBuilder) {
+  constructor(private contactService: ContactService, private fb: FormBuilder) {}
+
+  get languagesFormArray(): FormArray {
+    return this.contactForm.controls.languages as FormArray;
   }
 
-  get genders(): Gender[] {
-    return this._genders;
-  }
-
-
-  get languages(): Language[] {
-    return this._languages;
-  }
+  get genders(): Gender[] { return this._genders; }
 
   ngOnInit(): void {
+    const fb = this.fb;
     this.contactForm = new FormGroup({
-      firstName: new FormControl(''),
-      surname: new FormControl(''),
-      email: new FormControl('', [Validators.required, emailValidator]),
-      gender: new FormControl(''),
-      languages: this.fb.array([
-        this.fb.group({Java: false}),
-        this.fb.group({Kotlin: false}),
-        this.fb.group({Typescript: false}),
-        this.fb.group({PHP: false})
-      ])
+      firstName: fb.control(''),
+      surname: fb.control(''),
+      email: fb.control('', [Validators.required, emailValidator]),
+      gender: fb.control(''),
+      languages: fb.array([])
     });
+    this.addLanguages();
   }
 
-  addContactModelDriven(): void {
+  private addLanguages(): void {
+    this.languages.forEach(() => this.languagesFormArray.push(new FormControl(false)));
+  }
+
+  submit(): void {
+    this.contactForm.value.languages = this.contactForm.value.languages
+      .map((checked, i) => checked ? this.languages[i] : null)
+      .filter(v => v !== null);
+
     this.contactService.add(this.contactForm.value);
     this.contactForm.reset();
     this.toggleForm();
