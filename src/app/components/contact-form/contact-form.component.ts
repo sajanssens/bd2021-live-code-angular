87e32ee47ca596208c3141a6c49fea76f064d0bd
bramjanssens <bram.janssens@infosupport.com>
2021-12-14 15:41:05 +0100

    Checkboxes extended.

--- src/app/components/contact-form/contact-form.component.ts
+++ src/app/components/contact-form/contact-form.component.ts
@@ -4,6 +4,7 @@ import {ContactService} from '../../services/contact.service';
 import {Contact} from '../../domain/contact';
 import {Gender} from '../../domain/gender';
 import {emailValidator} from '../../util/util';
+import {Language} from '../../domain/language';
 
 @Component({
   selector: 'app-contact-form', // de html-tag die je gebruikt om dit component ergens in een ander component te plaatsen
@@ -16,6 +17,9 @@ export class ContactFormComponent implements OnInit {
   // tslint:disable-next-line:variable-name
   private _genders: Gender[] = [{id: 1, name: 'Male'}, {id: 2, name: 'Female'}];
 
+  // tslint:disable-next-line:variable-name
+  private _languages: Language[] = [{id: 1, name: 'Java'}, {id: 2, name: 'Kotlin'}, {id: 3, name: 'Typescript'}, {id: 4, name: 'PHP'},];
+
   contactForm: FormGroup;
   plusMinus = 'plus';
 
@@ -23,8 +27,12 @@ export class ContactFormComponent implements OnInit {
   }
 
   get genders(): Gender[] {
-    console.log(this._genders);
     return this._genders;
+  }
+
+
+  get languages(): Language[] {
+    return this._languages;
   }
 
   ngOnInit(): void {
@@ -34,10 +42,10 @@ export class ContactFormComponent implements OnInit {
       email: new FormControl('', [Validators.required, emailValidator]),
       gender: new FormControl(''),
       languages: this.fb.array([
-        this.fb.group({sel: false}),
-        this.fb.group({sel: false}),
-        this.fb.group({sel: false}),
-        this.fb.group({sel: false})
+        this.fb.group({Java: false}),
+        this.fb.group({Kotlin: false}),
+        this.fb.group({Typescript: false}),
+        this.fb.group({PHP: false})
       ])
     });
   }
